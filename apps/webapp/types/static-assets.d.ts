7e32eac24ca936fef888f870ad04c9827ff827a4
hwy <1165203865@qq.com>
2026-06-15 13:14:04 +0800

    fix(app): 🐛  include landing preview image

---
+++ apps/webapp/types/static-assets.d.ts
@@ -0,0 +1,17 @@
+// 静态资源类型声明，tsc --noEmit 需要这些声明来识别 Next.js 打包层处理的模块导入。
+declare module '*.png' {
+    const value: unknown
+    export default value
+}
+
+declare module '*.jpg' {
+    const value: unknown
+    export default value
+}
+
+declare module '*.svg' {
+    import type { FC, SVGProps } from 'react'
+    export const ReactComponent: FC<SVGProps<SVGSVGElement>>
+    const value: unknown
+    export default value
+}
